920089a3835319cc034fc8a6992c08a30eacb597
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2024-11-11 11:41:08 -0300

    feat: added toast animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <Todos @badValue="triggerToast" />
   </div>
 </template>
@@ -13,18 +15,76 @@ import Todos from '../components/Todos'
 export default {
   components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
+    const showP = ref(false);
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
+/* .toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+} */
 
+.toast-enter-active {
+  /* transition: all .3s ease; */
+  animation: wobble .5s ease;
+}
+
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-leave-active {
+  transition: all .5s ease;
+}
+
+@keyframes wobble {
+  0% {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+
+  50% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  60% {
+    transform: translateX(8px);
+  }
+
+  70% {
+    transform: translateX(-8px);
+  }
+
+  80% {
+    transform: translateX(4px);
+  }
+
+  90% {
+    transform: translateX(-4px);
+  }
+
+  100% {
+    transform: translateX(0);
+  }
+}
 </style>
