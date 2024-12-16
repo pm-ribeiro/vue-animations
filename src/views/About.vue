d28e1b82ee22fda0d47b8a1db47ba2e0867abd9a
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2024-12-16 10:43:37 -0300

    feat: added gsap

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,24 +2,35 @@
   <div class="about">
     <transition
       appear
-      name="fade"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
     >
       <h1 v-if="showTitle">About</h1>
     </transition>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Cum aperiam officia possimus delectus inventore quod quisquam
+      culpa voluptas iusto, quae maiores quo dolorum, corporis
+      laboriosam a dolore consequatur assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Cum aperiam officia possimus delectus inventore quod quisquam 
+      culpa voluptas iusto, quae maiores quo dolorum, corporis
+      laboriosam a dolore consequatur assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Cum aperiam officia possimus delectus inventore quod quisquam 
+      culpa voluptas iusto, quae maiores quo dolorum, corporis 
+      laboriosam a dolore consequatur assumenda nam!
+    </p>
   </div>
 </template>
 
 <script setup>
   import {ref} from 'vue';
+  import gsap from 'gsap';
 
   const showTitle = ref(true);
 
@@ -32,26 +43,6 @@
     console.log('enter', el);
   }
 
-  const afterEnter =  (el) => {
-    el.style.color = 'green';
-    console.log('afterEnter', el);
-
-    setTimeout(()=> showTitle.value = false, 2000)
-  }
-
-  const beforeLeave =  (el) => {
-    el.style.color = 'red';
-    console.log('beforeLeave', el);
-  }
-
-  const leave =  (el) => {
-    console.log('leave', el);
-  }
-
-  const afterLeave =  (el) => {
-    console.log('afterLeave', el);
-  }
-
 </script>
 
 <style>
@@ -60,11 +51,4 @@
     margin: 20px auto;
   }
 
-  .fade-enter-from, .fade-leave-to {
-    opacity: 0;
-  }
-
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .3s ease;
-  }
 </style>
