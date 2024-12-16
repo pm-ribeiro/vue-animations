46a07db7d587321e4c409bb0413f054874e14c40
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2024-12-16 11:24:56 -0300

    feat: added page animation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
     <router-link to="/about">About</router-link> | 
     <router-link to="/contact">Contact</router-link>
   </div>
-  <router-view/>
+
+  <router-view v-slot="{Component}">
+    <transition name="route" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+
 </template>
 
 <style>
@@ -25,4 +31,22 @@ body {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+/* route transitions */
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+
+/* controls the transitions properties */
+.route-enter-active, .route-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+
 </style>
